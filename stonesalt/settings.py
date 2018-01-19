3df824b904ec5fe756dbadb929478f0063198d12
naerthon <naerthon@gmail.com>
2018-01-19 11:10:04 -0200

    Update

--- stonesalt/settings.py
+++ stonesalt/settings.py
@@ -65,12 +65,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wsgi.application'
 
 
+from . import database
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': database.config()
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
